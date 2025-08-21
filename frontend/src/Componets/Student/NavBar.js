229cc48ca9db88d63bd06bb98efa7bde6514dc53
David Aphane <davidaphaneacademics@gmail.com>
2025-08-21 13:20:19 +0200

    Basic student codes added

--- frontend/src/Componets/Student/NavBar.js
+++ frontend/src/Componets/Student/NavBar.js
@@ -6,9 +6,8 @@ function NavBar(){
             <img src="/Amandla.png" alt="Example" class="Logo"/>
         <ul>
         <li><a class="active" href="#home">Dashboard</a></li>
-        <li><a href="#news">Create A Course</a></li>
-        <li><a href="#contact">Create New Quiz</a></li>
-        <li><a href="#about">Student Analysis</a></li>
+        <li><a href="#contact">Achievements</a></li>
+        <li><a href="#about">Analytics</a></li>
         <li><a href="#about">Quiz History</a></li>
 
         </ul>
---
+++ frontend/src/Views/Student/QuizHistory.js
@@ -0,0 +1,5 @@
+function QuizHistory() {
+  return <h1>Quiz History Page</h1>;
+}
+
+export default QuizHistory;
