f105c168580817e36af1cfc14d91e13ede80e50c
Asanda Ntuli <asqntuli7@gmail.com>
2025-09-06 04:53:09 +0200

    Adding the styling of the quiz and the dashboard

--- frontend/src/Views/Lacture/Dashboard.js
+++ frontend/src/Views/Lacture/Dashboard.js
@@ -1,6 +1,10 @@
+import Bio from "../../Componets/Lacture/bio";
+import Biography from "../../Componets/Lacture/Biography";
 import NavBar from "../../Componets/Lacture/NavBar";
+import QuizTile from "../../Componets/Lacture/QuizTile";
 import SearchBar from "../../Componets/Lacture/SearchBar";
 import "./Dashboard.css";
+
 
 function Dashboard() {
   return(
@@ -12,9 +16,30 @@ function Dashboard() {
          <SearchBar></SearchBar>
       </div>
       <div className="Container">
+        <div className="Boigraphy">
+          <Biography></Biography>
+
+        </div>
 
       </div>
       <div className="Side"></div>
+      <div className="Boi">
+        <Bio></Bio>
+      </div>
+      
+        <div className="Title">
+          Quiz List
+        </div>
+        <div className="More">
+          View All
+        </div>
+        <div className="QuizList">
+          <QuizTile></QuizTile>
+
+        </div>
+
+
+      
  
     </div> 
   
